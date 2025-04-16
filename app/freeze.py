7c308c303e7bfb71069ddddc68a1e8c09b03d047
robertovicario <robertovicario.com@gmail.com>
2025-04-16 19:11:15 +0200

    updates

--- app/freeze.py
+++ app/freeze.py
@@ -8,10 +8,6 @@ from app import app
 
 freezer = Freezer(app)
 
-dir_templates = os.path.join(os.path.dirname(__file__), 'templates')
-dir_build = os.path.join(os.path.dirname(__file__), 'build')
-shutil.copy(os.path.join(dir_templates, 'README.md'), os.path.join(dir_build, 'README.md'))
-
 # -------------------------
 
 if __name__ == '__main__':
